caf6e468127c5b2f81b4a20dd3b4179de09cd742
Giuseppe Bonocore <gbonocor@redhat.com>
2019-07-12 13:22:49 +0200

    adding file reading capabilities

---
+++ src/main/java/com/rh/pot/camel/Application.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2005-2016 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.rh.pot.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.servlet.CamelHttpTransportServlet;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(
+            new CamelHttpTransportServlet(), "/file-message-service/*");
+        servlet.setName("CamelServlet");
+        return servlet;
+    }
+
+    @Component
+    class RestApi extends RouteBuilder {
+
+        @Override
+        public void configure() {
+            restConfiguration()
+                .contextPath("/file-message-service").apiContextPath("/api-doc")
+                    .apiProperty("api.title", "Camel REST API")
+                    .apiProperty("api.version", "1.0")
+                    .apiProperty("cors", "true")
+                    .apiContextRouteId("doc-api")
+                .component("servlet")
+                .bindingMode(RestBindingMode.json);
+
+            rest("/file").description("File retriever rest service")
+                .get("/").description("File retriever rest service")
+                    .route().routeId("fms-api")
+                    .to("direct:fms")
+                    .endRest();
+
+          from("direct:fms")
+          .pollEnrich().simple("file:{{fms.fileDir}}?readLock=none&noop=true&idempotent=false&fileName={{fms.fileName}}")
+                    .log("FileContent: ${body}")
+                    .convertBodyTo(String.class); 
+                    //.transform().constant("Hello World"); 
+
+
+           from("servlet:///props").transform(simple(
+                        "fms.fileDir: {{fms.fileDir}} \n"
+                       + "fms.fileName: {{fms.fileName}}"));
+                   
+        }
+    }
+
+}
